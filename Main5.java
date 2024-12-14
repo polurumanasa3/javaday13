185f8500758c4c820540a443ddef5e01eea5b4c1
polurumanasa3 <manasap142@gmail.com>
2024-12-13 22:25:18 -0800

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+interface a
+{
+    public void r();
+}
+interface b
+{
+    public void r1();
+}
+class c implements a,b
+{
+    public void r()
+    {
+        System.out.println("Hello teachers");
+    }
+    public void r1()
+    {
+        System.out.println("Hello professors");
+    }
+}
+public class Main5
+{
+    public static void main(String args[])
+    {
+        c h1=new c();
+        h1.r();
+        h1.r1();
+
+    }
+}
+    
+    
+
+    
+
